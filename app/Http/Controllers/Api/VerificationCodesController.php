6e13aa41a2024aea6fa4077b3ad049ad6b77297b
huizai <237002893@qq.com>
2020-08-24 17:29:27 +0000

    图片验证码

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -11,9 +11,21 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
 
-        if(!app()->environment('production')) {
+        if (!$captchaData) {
+            abort(403, '图片验证码已失效');
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            throw new AuthenticationException('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
+        if (!app()->environment('production')) {
             $code = '1234';
         } else {
             // 生成4位随机数，左侧补0
@@ -32,11 +44,12 @@ class VerificationCodesController extends Controller
             }
         }
 
-
         $key = 'verificationCode_'.Str::random(15);
         $expiredAt = now()->addMinutes(5);
-        // 缓存验证码 5 分钟过期。
+        // 缓存验证码 5分钟过期。
         \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return response()->json([
             'key' => $key,
